46a8e3ac133d988ded89b45e594ac2e7e84ca8ac
Vid Ernečić <vidernecic1@gmail.com>
2026-08-18 17:24:17 +0200

    fetch trips, single trip

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import "dotenv/config";
 import express from "express";
 import cors from "cors";
 import { connectToDatabase } from "./db.js";
+import { ObjectId } from "mongodb";
 
 const PORT = process.env.PORT || 8000;
 const app = express();
@@ -22,6 +23,57 @@ app.get("/api/trips", async (req, res) => {
   } catch (error) {
     console.error("Greška pri dohvaćanju izleta:", error);
     return res.status(500).json({ message: "Greška pri dohvaćanju izleta" });
+  }
+});
+
+app.get("/api/trips/:id", async (req, res) => {
+  const { id } = req.params;
+  if (!ObjectId.isValid(id)) {
+    return res.status(400).json({ message: "Neispravan ID izleta" });
+  }
+  try {
+    const trip = await db
+      .collection("Trips")
+      .findOne({ _id: new ObjectId(id) });
+    if (!trip) {
+      return res.status(404).json({ message: "Izlet nije pronađen" });
+    }
+    const { _id, ...rest } = trip;
+    return res.status(200).json({ id: _id.toString(), ...rest });
+  } catch (error) {
+    console.error("Greška pri dohvaćanju izleta:", error);
+    return res.status(500).json({ message: "Greška pri dohvaćanju izleta" });
+  }
+});
+
+app.post("/api/trips", async (req, res) => {
+  const {
+    title,
+    description,
+    shortDescription,
+    difficulty,
+    maxParticipants,
+    country,
+    elevation,
+    location,
+  } = req.body;
+
+  const trip = {
+    title,
+    description,
+    shortDescription,
+    difficulty,
+    maxParticipants,
+    country,
+    elevation,
+    location,
+  };
+  try {
+    await db.collection("Trips").insertOne(trip);
+    return res.status(201).json({ message: "Izlet je uspješno kreiran" });
+  } catch (error) {
+    console.error("Greška pri kreiranju izleta:", error);
+    return res.status(500).json({ message: "Greška pri kreiranju izleta" });
   }
 });
 
